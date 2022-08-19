6bff5406a0522ebb0f7553b489b9a0a97f53d54a
Legit15prince <legitpr15@gmail.com>
2022-08-19 12:03:46 +0100

    first commit

---
+++ custom.js
@@ -0,0 +1,73 @@
+
+// JS STYLING
+// var shape = document.getElementById('container');
+// shape.style.border ='2px solid black';
+// shape.style.height ='70vh';
+// shape.style.width ='100vw';
+// shape.style.borderRadius ='100px';
+
+// JS OUTPUTS
+// document.write('hello world')
+// window.alert('hello world')
+// prompt('hello world')
+// console.log('hello world')
+// document.getElementById('demo').innerHTML ='hello world'
+
+// JS OPERATORS
+// arithemetic operators e.g addition (+), substraction (-), multiplication(*), division(/), modulus (%)
+
+// comparison operators e.g equal ==, stricty equal to ===, greater than >, less than <, greater than or equal to >=, 
+// less than or equal to <=, not equal to !==, not strictly equal to !===
+
+// logical operators or || and &&
+
+// assignment operator equal sign =
+
+
+// function hhh() {
+//     document.body.style.background ='blue';
+// }
+
+// CONDITIONAL STATMENTS
+// if else 
+
+// var number = 1;
+// if (number > 5) {
+//     console.log("false")
+// } 
+
+// else {
+//     console.log("we didnot meet any of nthe staement")
+// }
+
+// var phone = prompt('what is the name of your phone');
+
+// if (phone=== 'infinix') {
+//     alert('nice phone');
+// }
+// else if (phone === 'vivo') {
+//     alert('quality phone');
+// }
+// else {
+//     alert('fucking phone');
+// }
+
+chioma=20;
+if(chioma==20){
+    alert('yes she is 20 years old')
+}
+else{
+    alert('no she is younger')
+}
+var party=prompt('how old are you');
+if(party < 18){
+    alert('go back home')
+}
+else if (party>18 && party <=25){
+    alert('na you get this party')
+}
+
+
+
+
+
